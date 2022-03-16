4d05e42d27563a7c8b4781bb68b8e4eb7cf05c19
David Pätzel <david.paetzel@posteo.de>
2022-03-16 12:45:12 +0100

    Add working ale-py

--- flake.nix
+++ flake.nix
@@ -3,15 +3,20 @@
 
   inputs = {
     # nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-21.11";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     # nixpkgs.url =
     #   "github:NixOS/nixpkgs/bc5d68306b40b8522ffb69ba6cff91898c2fbbff";
+
     # This is where importlib-metadata was bumped to 4.10.0
     nixpkgs-importlib-metadata.url =
       "github:NixOS/nixpkgs/2a0966e8165fe41efe4d150dc0bd95c7b2d67256";
+
     mach-nix.url =
-      "github:DavHau/mach-nix/31b21203a1350bff7c541e9dfdd4e07f76d874be";
+      # "github:DavHau/mach-nix/31b21203a1350bff7c541e9dfdd4e07f76d874be";
+      "github:DavHau/mach-nix/";
     mach-nix.inputs.nixpkgs.follows = "nixpkgs";
+
     ale-py-source.url = "github:mgbellemare/Arcade-Learning-Environment/v0.7.4";
     ale-py-source.flake = false;
   };
@@ -39,6 +44,9 @@
       python = pkgs.python39;
       pythonStr = "python39";
 
+      importlib-metadata =
+        pkgsImportlibMetadata.${pythonStr}.pkgs.importlib-metadata;
+
       cudatoolkit = pkgs.cudaPackages.cudatoolkit_11_2;
       cudnn = pkgs.cudnn_cudatoolkit_11_2;
       nvidia = pkgs.linuxPackages.nvidia_x11;
@@ -53,32 +61,43 @@
 
         # The file referenced here *must* be part of this flake (i.e. checked
         # into the same Git repository). Also, you *must* use a relative path.
-        requirements = builtins.readFile ./requirements.txt;
+        # requirements = builtins.readFile ./requirements.txt;
+        requirements = "#";
 
         # Select providers for dependencies that should not be installed from
         # PyPI or conda (e.g. ones that rely on system libraries such as
         # cudatoolkit).
         providers.torch = "nixpkgs";
-        providers.importlib-metadata = "nixpkgs";
+        # providers.numpy = "nixpkgs";
 
         # Non-Python dependencies.
-        packagesExtra = let
-          importlib-metadata =
-            pkgsImportlibMetadata.${pythonStr}.pkgs.importlib-metadata;
-        in [
+        packagesExtra = [
           (machNix.buildPythonPackage {
             pname = "ale-py";
             version = "v0.7.4";
             src = ale-py-source;
-            requirements = "matplotlib";
+            requirements = "#";
             nativeBuildInputs = with pkgs; [ cmake git ];
-            propagatedBuildInputs = [ pkgs.zlib importlib-metadata ] ++ (with python.pkgs; [
-              importlib-resources
-              numpy
-              markdown
-              zipp
-            ]);
+            propagatedBuildInputs = [ pkgs.zlib importlib-metadata ]
+              ++ (with python.pkgs; [
+                importlib-resources
+                numpy
+                markdown
+                zipp
+              ]);
             dontUseCmakeConfigure = true;
+            # I think we have a similar problem like these ones
+            #
+            # - https://github.com/NixOS/nixpkgs/issues/75125
+            #
+            # - https://github.com/NixOS/nixpkgs/issues/84774
+            #
+            # The importlib-metadata package somehow isn't found by the
+            # setup.py albeit being there. We thus simply remove that line from
+            # setup.py as a dirty workaround.
+            postPatch = ''
+                sed -i "s/.*importlib-metadata.*//" setup.cfg
+            '';
             patches = [
               ./0001-Fix-parse_version-in-setup.py.patch
               ./0001-Disable-SDL.patch
@@ -90,12 +109,6 @@
             ];
           })
         ];
-
-        # overridesPre = [
-        #   (final: prev: rec {
-        #     importlib-metadata = pkgsImportlibMetadata.${pythonStr}.pkgs.importlib-metadata;
-        #   })
-        # ];
 
         # Configure nixpkgs dependencies if necessary.
         overridesPost = [
